e737bd83bc0fc7c75064f50aa8b3190679adedcc
yuizhixiaocaiji <1390683299@qq.com>
2023-05-26 00:01:59 +0800

    初始化vue配置

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/user": {
+        target: "http:localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/user/, ""),
+      },
+    },
+  },
+});
