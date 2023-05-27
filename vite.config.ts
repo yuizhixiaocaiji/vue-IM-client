91fde1d719c10db721cb5246393d5093120801a3
yuizhixiaocaiji <1390683299@qq.com>
2023-05-27 10:06:31 +0800

    修复ts识别不了.vue文件的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
@@ -20,5 +26,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [ElementPlus()],
 });
