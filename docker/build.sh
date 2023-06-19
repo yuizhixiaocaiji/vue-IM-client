7cc1aed4d90f422ef42e43d43e2a05ce11272901
yuizhixiaocaiji <1390683299@qq.com>
2023-06-19 23:26:00 +0800

    修复history下刷新页面404的问题

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,4 @@
 npm run build
 cp -r ../dist ./
-docker build -t ccr.ccs.tencentyun.com/yizhixiaocaiji/goim-client:v0.0.3 ./
-docker push ccr.ccs.tencentyun.com/yizhixiaocaiji/goim-client:v0.0.3
+docker build -t ccr.ccs.tencentyun.com/yizhixiaocaiji/goim-client:v0.0.5 ./
+docker push ccr.ccs.tencentyun.com/yizhixiaocaiji/goim-client:v0.0.5
